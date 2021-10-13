2a836264105e58c8cb5b42adb213c37eacb4c1f0
Tayyip GÜZEL <86917901+tyypgzl@users.noreply.github.com>
2021-10-14 00:04:56 +0300

    new project

---
+++ lib/feature/news/view/news_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NewsView extends StatefulWidget {
+  NewsView({Key? key}) : super(key: key);
+
+  @override
+  _NewsViewState createState() => _NewsViewState();
+}
+
+class _NewsViewState extends State<NewsView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
